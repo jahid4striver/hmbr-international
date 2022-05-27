e648119c52873c6296eb95bfe0670932a3c94f26
jahid4striver <jahidhassn701@gmail.comr>
2022-05-28 03:49:41 +0600

    firebase keys hidden

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,19 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqTbJfebhheyFMEW5Q5w7YdFxPWvDWPnA",
-  authDomain: "hmbr-international.firebaseapp.com",
-  projectId: "hmbr-international",
-  storageBucket: "hmbr-international.appspot.com",
-  messagingSenderId: "391508607751",
-  appId: "1:391508607751:web:09cf4f6dce6eb7c620fba2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTED_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  
+  // apiKey: "AIzaSyCqTbJfebhheyFMEW5Q5w7YdFxPWvDWPnA",
+  // authDomain: "hmbr-international.firebaseapp.com",
+  // projectId: "hmbr-international",
+  // storageBucket: "hmbr-international.appspot.com",
+  // messagingSenderId: "391508607751",
+  // appId: "1:391508607751:web:09cf4f6dce6eb7c620fba2"
 };
 
 // Initialize Firebase
